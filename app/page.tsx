0555cee445a51c78c6b981f992fbb7f2055de6eb
Gtafive11220.ranaharoon@gmail.com <0.ranaharoon@gmail.com>
2025-07-29 12:05:01 +0500

    pic update

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Page() {
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 hover:grayscale-0"
           unoptimized
-          width={160}
-          height={160}
+          width={300}
+          height={300}
           priority
         />
       </a>
